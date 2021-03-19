624e325ccfb3b81583d6b02cc88e9aca24b60629
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-03-19 10:00:55 +0100

    Updates

--- SOURCE/MOD_20_Test-Application/MOD_23_Register-itself-at-the-OI4-Service-Registry/registeratOI4.js
+++ SOURCE/MOD_20_Test-Application/MOD_23_Register-itself-at-the-OI4-Service-Registry/registeratOI4.js
@@ -1,5 +1,64 @@
 var mqtt = require('mqtt')
+var crypto = require('crypto')
+var client
+
+const SerialNumber = 'undefined'
+const Model = 'DNS_SD_Test_Application'
+const Productcode = 'DNS_SD_TEST'
+const oi4Identifier = 'urn:undefined/' + Model + '/' + Productcode + '/' + SerialNumber
+
 
 module.exports.start = function() {
-    
+    // Send mam to: 'oi4/Aggregation/<appId>/pub/mam/<oi4Identifier>'
+    client = mqtt.connect('mqtt://localhost')
+    client.on('connect', () => {
+        client.publish('oi4/Aggregation/' + oi4Identifier + '/pub/mam' + oi4Identifier, buildmsg(buildmamMessage()))
+    })
+    client.on('message', (topic, message) => {
+
+    })
+}
+
+function buildmamMessage() {
+    var mam = {
+        DataSetWriterId: oi4Identifier,
+        Timestamp: "<DateTime>",
+        Payload: { 
+            Manufacturer: { 
+                Locale: "<localeId>",
+                Text: "<String>" 
+            }, 
+            ManufacturerUri: "",
+            Model: { 
+                Locale: "<localeId>", 
+                Text: "<String>" 
+            },
+            ProductCode: "",
+            HardwareRevision: "",
+            SoftwareRevision: "",
+            DeviceRevision: "", 
+            DeviceManual: "",
+            DeviceClass: "",
+            SerialNumber: "",
+            ProductInstanceUri: oi4Identifier,
+            RevisionCounter: '<INT32>',
+            Description: { 
+                Locale: "<localeId>",
+                Text: "<String>" 
+            } 
+        }
+    }
+    return mam
+}
+
+function buildmsg(messages) {
+    var msgWrapper = {
+        MessageId: 'unixTimestampInMs-PublisherId',
+        MessageType: 'ua-data',
+        PublisherId: '<serviceType>/<appId>',
+        DataSetClassId: '360ca8f3-5e66-42a2-8f10-9cdf45f4bf58',
+        CorrelationId: '<empty/not present> or <initial MessageId>',
+        Messages: messages
+    }
+    return JSON.stringify(msgWrapper)
 }
--- SOURCE/MOD_20_Test-Application/main.js
+++ SOURCE/MOD_20_Test-Application/main.js
@@ -1,4 +1,3 @@
-
 var webInterface = require("./MOD_21_Web-Interface/webInterface")
 var dnssdListener = require("./MOD_24_DNS-SD-listener/dnssdListener")
 var announceService = require("./MOD_22_Announce_Service_via_DNS-SD/announceService")
