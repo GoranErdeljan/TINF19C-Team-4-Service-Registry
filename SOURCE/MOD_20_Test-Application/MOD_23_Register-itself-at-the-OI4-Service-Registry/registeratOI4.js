d4c562cead2ef8238ca122c4e71f5d1a84e8fbb5
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-03-30 15:17:01 +0200

    Added exit

--- SOURCE/MOD_20_Test-Application/MOD_23_Register-itself-at-the-OI4-Service-Registry/registeratOI4.js
+++ SOURCE/MOD_20_Test-Application/MOD_23_Register-itself-at-the-OI4-Service-Registry/registeratOI4.js
@@ -38,6 +38,7 @@ module.exports.start = function() {
                     healthState: 0
                 }
             }], "360ca8f3-5e66-42a2-8f10-9cdf45f4bf58"))
+            process.exit()
         }
         process.on('exit', exitHandler.bind());
         process.on('SIGINT', exitHandler.bind());
