2823e10676a8591ed906d0c9dea58e5ed7f81944
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-05 11:41:03 +0200

    Updates

--- SOURCE/MOD_20_Test-Application/MOD_21_Web-Interface/wwwroot/js/scripts.js
+++ SOURCE/MOD_20_Test-Application/MOD_21_Web-Interface/wwwroot/js/scripts.js
@@ -1,46 +1,47 @@
-$.get('/DNSSD/Entries', {}, function(data) {
-    data = JSON.parse(data)
-    data.forEach(element => {
-        console.log(element.answers)
+setInterval(getDNSSDEntries(), 60000)
+getDNSSDEntries()
+function getDNSSDEntries() {
+    $.get('/DNSSD/Entries', {}, function (data) {
+        data = JSON.parse(data)
+        data.forEach(element => {
+            console.log(element.answers)
 
-        var srv = []
-        var a = []
-        var txt = []
+            var srv = []
+            var a = []
+            var txt = []
 
-        element.answers.forEach(answer => {
-            if (answer.type == 'SRV')
-            {
-                srv.push(answer.name)
-            }
-            if (answer.type == 'A' || answer.type == 'AAAA')
-            {
-                a.push(answer.data)
-            }
-            if (answer.type == 'TXT')
-            {
-                txt.push(JSON.stringify(answer.data))
-            }
+            element.answers.forEach(answer => {
+                if (answer.type == 'SRV') {
+                    srv.push(answer.name)
+                }
+                if (answer.type == 'A' || answer.type == 'AAAA') {
+                    a.push(answer.data)
+                }
+                if (answer.type == 'TXT') {
+                    txt.push(JSON.stringify(answer.data))
+                }
+            })
+
+            var tr = document.createElement("tr")
+            var td = document.createElement("td")
+            srv.forEach(element => {
+                td.appendChild(document.createTextNode(element + " "))
+            })
+            tr.appendChild(td);
+
+            td = document.createElement("td")
+            a.forEach(element => {
+                td.appendChild(document.createTextNode(element + " "))
+            })
+            tr.appendChild(td);
+
+            td = document.createElement("td")
+            txt.forEach(element => {
+                td.appendChild(document.createTextNode(element + " "))
+            })
+            tr.appendChild(td);
+
+            dnssd_Entries_List.firstElementChild.appendChild(tr);
         })
-        
-        var tr = document.createElement("tr")
-        var td = document.createElement("td")
-        srv.forEach(element => {
-            td.appendChild(document.createTextNode(element + " "))
-        })
-        tr.appendChild(td);
-
-        td = document.createElement("td")
-        a.forEach(element => {
-            td.appendChild(document.createTextNode(element + " "))
-        })
-        tr.appendChild(td);
-
-        td = document.createElement("td")
-        txt.forEach(element => {
-            td.appendChild(document.createTextNode(element + " "))
-        })
-        tr.appendChild(td);
-
-        dnssd_Entries_List.firstElementChild.appendChild(tr);
     })
-})
+}
--- SOURCE/MOD_20_Test-Application/MOD_24_DNS-SD-listener/dnssdListener.js
+++ SOURCE/MOD_20_Test-Application/MOD_24_DNS-SD-listener/dnssdListener.js
@@ -3,16 +3,17 @@ var mdns = require('multicast-dns')();
 var callbacks = []
 
 module.exports.start = function () {
+    setInterval(() => {
     mdns.query({
         questions: [{
             name: '',
             type: 'A'
         }]
-    })
+    })}, 60000)
 
     mdns.on('response', function (query) {
-        //console.log("Response: ")
-        //console.log(query)
+        console.log("DNS SD Response: ")
+        console.log(query)
         callbacks.forEach(cb => {
             cb(query)
         })
