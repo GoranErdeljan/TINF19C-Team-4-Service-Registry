ae9c43c53b17a64eeee156552a0ba06ebc146b72
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-16 15:36:56 +0200

    Tests

--- SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/main.js
+++ SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/main.js
@@ -2,6 +2,28 @@ var validator = require("./MOD_13_OI4-Conformity-Validator/oi4ConformityValidato
 var registry = require('./MOD_11_DNS-SD-Listener/addtoRegistry')
 
 registry.start(undefined, undefined, () => {
-    console.log('connected')
-    registry.addDevice("urn:undefined.com/Test/Test/undefined", { blabla: 'blabla' }, Date.now() + 70000)
+    registry.addDevice("urn:undefined.com/Test/Test/undefined", {
+        Manufacturer: {
+            Locale: "de-de",
+            Text: "TINF19C-Team4"
+        },
+        ManufacturerUri: "urn:undefined.com",
+        Model: {
+            Locale: "de-de",
+            Text: Model
+        },
+        ProductCode: Productcode,
+        HardwareRevision: "",
+        SoftwareRevision: "0.0",
+        DeviceRevision: "",
+        DeviceManual: "Not available",
+        DeviceClass: DeviceClass,
+        ProductInstanceUri: oi4Identifier,
+        RevisionCounter: 1,
+        SerialNumber: SerialNumber,
+        Description: {
+            Locale: "de-de",
+            Text: Model
+        }
+    }, Date.now() + 70000)
 })
