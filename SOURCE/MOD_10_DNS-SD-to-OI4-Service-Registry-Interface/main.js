3b825dbe34fbae9aad4a0d2eb40ec9b0df9010e0
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-16 15:34:21 +0200

    updates

--- SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/main.js
+++ SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/main.js
@@ -1,7 +1,7 @@
 var validator = require("./MOD_13_OI4-Conformity-Validator/oi4ConformityValidator")
 var registry = require('./MOD_11_DNS-SD-Listener/addtoRegistry')
 
-registry.start(() => {
+registry.start(undefined, undefined, () => {
     console.log('connected')
     registry.addDevice("urn:undefined.com/Test/Test/undefined", { blabla: 'blabla' }, Date.now() + 70000)
 })
