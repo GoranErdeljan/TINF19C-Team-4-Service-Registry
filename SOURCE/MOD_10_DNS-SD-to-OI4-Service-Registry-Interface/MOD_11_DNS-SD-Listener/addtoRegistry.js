65bff57da0f461b597d07e62ebf56e64b4b47a52
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-17 15:07:47 +0200

    tests

--- SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/MOD_11_DNS-SD-Listener/addtoRegistry.js
+++ SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/MOD_11_DNS-SD-Listener/addtoRegistry.js
@@ -75,6 +75,7 @@ module.exports.start = function (hostname = "localhost", port = 1883, connectcb 
             Object.keys(devices).forEach(device => {
                 if (topic.endsWith(devices[device].oi4Identifier))
                 {
+                    console.log(devices[device].oi4Identifier + " was addressed")
                     if (topic.includes("get/mam"))
                     {
                         client.publish('oi4/Device/' + devices[device].oi4Identifier + '/pub/mam/' + oi4Identifier, 
