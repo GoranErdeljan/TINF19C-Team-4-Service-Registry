280b78b42abd1e58430c663bf42ccd7da48c6016
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-23 08:27:03 +0200

    updates

--- SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/main.js
+++ SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/main.js
@@ -4,30 +4,6 @@ var dnssdListener = require('./MOD_11_DNS-SD-Listener/dnssdListener')
 
 
 registry.start(undefined, undefined, () => {
-    registry.addDevice("urn:undefined.com/Test/Test/undefined", {
-        Manufacturer: {
-            Locale: "de-de",
-            Text: "TINF19C-Team4"
-        },
-        ManufacturerUri: "urn:undefined.com",
-        Model: {
-            Locale: "de-de",
-            Text: "Test"
-        },
-        ProductCode: "Test",
-        HardwareRevision: "",
-        SoftwareRevision: "0.0",
-        DeviceRevision: "",
-        DeviceManual: "Not available",
-        DeviceClass: "Device",
-        ProductInstanceUri: "urn:undefined.com/Test/Test/undefined",
-        RevisionCounter: 1,
-        SerialNumber: "",
-        Description: {
-            Locale: "de-de",
-            Text: "Test"
-        }
-    }, Date.now() + 70000)
 })
 
 
@@ -35,11 +11,13 @@ dnssdListener.start()
 dnssdListener.addCallback((response) => {
 
     let txtrecords = []
+    let ttl
 
     response.answers.forEach(answer => {
         if (answer.type == 'TXT') {
             answer.data.forEach(buffer => {
                 txtrecords.push(buffer.toString())
+                ttl = answer.ttl
             })
         }
     })
@@ -48,5 +26,5 @@ dnssdListener.addCallback((response) => {
 
     console.log("Found service")
     console.log(mam)
-    registry.addDevice(mam.ProductInstanceUri, mam)
+    registry.addDevice(mam.ProductInstanceUri, mam, ttl)
 })
