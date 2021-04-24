e296551f1e008753b4c45ca87fcc3e62a973e3d6
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-24 10:58:42 +0200

    Fixes

--- SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/main.js
+++ SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/main.js
@@ -1,7 +1,7 @@
 var validator = require("./MOD_13_OI4-Conformity-Validator/oi4ConformityValidator")
 var registry = require('./MOD_11_DNS-SD-Listener/addtoRegistry')
 var dnssdListener = require('./MOD_11_DNS-SD-Listener/dnssdListener')
-var announcer = require(".MOD_12_OI4-Service-Registry-listener/oi4Listener.js")
+var announcer = require("./MOD_12_OI4-Service-Registry-listener/oi4Listener.js")
 
 registry.start(undefined, undefined, () => {
 })
