36720c864295230255e57fa6b1280541457c437f
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-24 15:34:39 +0200

    Updates

--- SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/MOD_12_OI4-Service-Registry-listener/oi4Listener.js
+++ SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/MOD_12_OI4-Service-Registry-listener/oi4Listener.js
@@ -87,6 +87,9 @@ module.exports.start = ( connectcb = () => { }) => {
             })
         }
     })
+    setInterval(() => {
+        getHealthOfDevices()
+    }, 60000);
 }
 
 function getHealthOfDevices() {
