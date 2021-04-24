4f1ff485d34eff49395a78529ac7dffae45076ee
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-24 11:13:44 +0200

    Updates

--- SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/MOD_12_OI4-Service-Registry-listener/oi4Listener.js
+++ SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/MOD_12_OI4-Service-Registry-listener/oi4Listener.js
@@ -5,4 +5,17 @@ var client
 
 module.exports.start = (hostname = "localhost", port = 1883, connectcb = () => { }) => {
     client = mqtt.connect([{ host: hostname, port: port }])
+    client.on("connect", () => {
+        connectcb()
+        client.subscribe("oi4/urn:hilscher.com/Registry%20Application/OI4-REG/undefined/#", (err) => {
+            console.error(err)
+        })
+    })
+    client.on("error", (error) => {
+        console.error(error)
+    })
+    client.on("message", (topic, message) => {
+        console.log("Registry-Listener got:")
+        console.log(message)
+    })
 }
