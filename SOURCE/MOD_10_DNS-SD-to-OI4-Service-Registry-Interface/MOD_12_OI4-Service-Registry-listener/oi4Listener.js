c24abfc39ce8267e2dce44e595fd76c47943905e
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-04-26 22:05:10 +0200

    Refactoring

--- SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/MOD_12_OI4-Service-Registry-listener/oi4Listener.js
+++ SOURCE/MOD_10_DNS-SD-to-OI4-Service-Registry-Interface/MOD_12_OI4-Service-Registry-listener/oi4Listener.js
@@ -1,53 +1,55 @@
 var mqtt = require("mqtt")
 var mdns = require("multicast-dns")()
 
-const typeArray = ["", "_oi4-servicediscovery", "_http", "_tcp", "local"]
+const _typeArray = ["", "_oi4-servicediscovery", "_http", "_tcp", "local"]
 
-var mams = {}
+var _mams = {}
 
-var client
+var _client
 
-var config = undefined
+var _config = undefined
 
-var validator = undefined
+var _validator = undefined
 
-module.exports.setConfig = function (setconfig) {
-    config = setconfig
+module.exports.setConfig = function (config) {
+    _config = config
 }
-module.exports.setValidator = function (setValidator) {
-    validator = setValidator
+
+module.exports.setValidator = function (validator) {
+    _validator = validator
 }
+
 module.exports.start = (connectcb = () => { }) => {
-    if (typeof validator === 'undefined') {
+    if (typeof _validator === 'undefined') {
         console.error("[oi4Listener] No Validator specified")
         return
     }
-    if (typeof config === 'undefined') {
+    if (typeof _config === 'undefined') {
         console.error("[oi4Listener] No config specified")
         return
     }
-    client = mqtt.connect([{ host: config.mqtthost, port: config.mqttport }])
-    client.on("connect", () => {
+    _client = mqtt.connect([{ host: config.mqtt.hostname, port: config.mqtt.port }])
+    _client.on("connect", () => {
         connectcb()
-        client.subscribe("oi4/+/+/+/+/+/pub/mam/#", (err) => {
+        _client.subscribe("oi4/+/+/+/+/+/pub/mam/#", (err) => {
             console.log("[oi4Listener] Subscribed to 'oi4/+/+/+/+/+/pub/mam/#'")
             if (err)
                 console.error(err)
         })
     })
-    client.on("error", (error) => {
+    _client.on("error", (error) => {
         console.error("[oi4Listener] " + error)
     })
-    client.on("message", (topic, message) => {
+    _client.on("message", (topic, message) => {
         // Add MAM to mams
         // TODO: Add checks for undefined values        
         let messageobject = JSON.parse(message)
-        if (messageobject.PublisherId !== config.oi4.DeviceClass + "/" + config.oi4.oi4Identifier) {
+        if (messageobject.PublisherId !== _config.oi4.DeviceClass + "/" + _config.oi4.oi4Identifier) {
             messageobject.Messages.forEach(innerMessage => {
-                mams[innerMessage.Payload.ProductInstanceUri] = { mam: innerMessage.Payload, PublisherId: messageobject.PublisherId }
+                _mams[innerMessage.Payload.ProductInstanceUri] = { mam: innerMessage.Payload, PublisherId: messageobject.PublisherId }
                 console.log("[oi4Listener] MAM-Listener added: " + innerMessage.Payload.ProductInstanceUri)
                 console.log("[oi4Listener] MAMs are now: ")
-                console.log(Object.keys(mams))
+                console.log(Object.keys(_mams))
                 console.log()
             })
         }
@@ -65,28 +67,28 @@ module.exports.start = (connectcb = () => { }) => {
         })
         if (addressed) {
             console.log("[oi4Listener] Service was addressed, sending mdns responses \n")
-            Object.keys(mams).forEach(key => {
+            Object.keys(_mams).forEach(key => {
 
                 mdns.respond({
                     answers: [{
-                        name: mams[key].mam.ProductInstanceUri,
+                        name: _mams[key].mam.ProductInstanceUri,
                         type: 'SRV',
                         data: {
-                            port: config.hostport,
+                            port: _config.mdns.hostport,
                             weigth: 0,
                             priority: 10,
                             target: 'localhost'
                         }
                     }, {
-                        name: mams[key].mam.ProductInstanceUri + '._oi4-servicediscovery._http._tcp',
+                        name: _mams[key].mam.ProductInstanceUri + '._oi4-servicediscovery._http._tcp',
                         type: 'A',
                         ttl: 60,
-                        data: config.hostip
+                        data: _config.mdns.hostip
                     }, {
-                        name: mams[key].mam.ProductInstanceUri + '._oi4-servicediscovery._http._tcp',
+                        name: _mams[key].mam.ProductInstanceUri + '._oi4-servicediscovery._http._tcp',
                         type: 'TXT',
                         ttl: 60,
-                        data: buildTXTOfMAM(mams[key].mam)
+                        data: buildTXTOfMAM(_mams[key].mam)
                     }]
                 })
             })
@@ -96,8 +98,8 @@ module.exports.start = (connectcb = () => { }) => {
 }
 
 function monitorHealthOfDevices() {
-    let statusUnknown = Object.keys(mams)
-    let tempMqttClient = mqtt.connect([{ host: config.mqtthost, port: config.mqttport }])
+    let statusUnknown = Object.keys(_mams)
+    let tempMqttClient = mqtt.connect([{ host: _config.mqtt.hostname, port: _config.mqtt.port }])
     tempMqttClient.subscribe("oi4/+/+/+/+/+/pub/health/#", (err) => {
         if (err)
             console.error("[oi4Listener] " + err)
@@ -107,20 +109,20 @@ function monitorHealthOfDevices() {
             console.log("[oi4Listener] Stop waiting for Health messages, removing " + statusUnknown.length + ": ")
             statusUnknown.forEach(oi4Identifier => {
                 console.log(oi4Identifier)
-                delete mams[oi4Identifier]
+                delete _mams[oi4Identifier]
             })
-            statusUnknown = Object.keys(mams)
+            statusUnknown = Object.keys(_mams)
         }, 60000)
     })
     tempMqttClient.on("message", (topic, message) => {
         statusUnknown.forEach(key => {
             console.log("[oi4Listener] Got Health message from: ")
-            console.log(mams[key])
-            if (typeof mams[key] !== 'undefined')
-                if (topic.includes(mams[key].PublisherId)
-                    && topic.includes(mams[key].mam.ProductInstanceUri)) {
-                    console.log("[oi4Listener] " + mams[key].mam.ProductInstanceUri + " is ok")
-                    let index = statusUnknown.indexOf(mams[key].mam.ProductInstanceUri)
+            console.log(_mams[key])
+            if (typeof _mams[key] !== 'undefined')
+                if (topic.includes(_mams[key].PublisherId)
+                    && topic.includes(_mams[key].mam.ProductInstanceUri)) {
+                    console.log("[oi4Listener] " + _mams[key].mam.ProductInstanceUri + " is ok")
+                    let index = statusUnknown.indexOf(_mams[key].mam.ProductInstanceUri)
                     statusUnknown.splice(index, 1)
                 }
 
